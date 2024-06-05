af0a5b518b475f12cc5e90d820d80b5a6e3ecb8c
denisvlem <denisvlem@gmail.com>
2024-06-05 13:14:00 +0300

    Задача с компонентами связности. Решение через список связности

--- src/main/java/com/denisvlem/yandex/coderun/algo/CohesionComponent.java
+++ src/main/java/com/denisvlem/yandex/coderun/algo/CohesionComponent.java
@@ -4,10 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public class CohesionComponent {
 
@@ -18,100 +19,74 @@ public class CohesionComponent {
       var n = Integer.parseInt(nmArray[0]);
       var m = Integer.parseInt(nmArray[1]);
 
-      var graph = new Graph(n);
-      var vertexSet = new ArrayList<Integer>();
+      var adjustmentList = new HashMap<Integer, List<Integer>>();
+
       for (int i = 0; i < n; ++i) {
-        graph.addVertex(i + 1);
-        vertexSet.add(i+1);
+        adjustmentList.put(i, new ArrayList<>());
       }
 
       for (int i = 0; i < m; i++) {
         var edgeArray = reader.readLine().split(" ");
-        graph.addEdge(
-            Integer.parseInt(edgeArray[0]) - 1,
-            Integer.parseInt(edgeArray[1]) - 1
+
+        var from = Integer.parseInt(edgeArray[0]) - 1;
+        var to = Integer.parseInt(edgeArray[1]) - 1;
+
+        adjustmentList.get(from).add(to);
+        adjustmentList.get(to).add(from);
+      }
+
+      var components = new int[n];
+      Arrays.fill(components, -1);
+
+      int componentCount = 1;
+      for (int i = 0; i < components.length; ++i) {
+        if (components[i] == -1) {
+          dfs(adjustmentList, i, components, componentCount);
+          componentCount += 1;
+        }
+      }
+
+      System.out.println(componentCount-1);
+
+      var componentsMap = new HashMap<Integer, List<Integer>>();
+      for (int i = 1; i < componentCount; i++) {
+        componentsMap.put(i, new ArrayList<>());
+      }
+
+      for (int i = 0; i < components.length; ++i) {
+        componentsMap.get(components[i]).add(i + 1);
+      }
+
+      for (var component : componentsMap.entrySet()) {
+        System.out.println(component.getValue().size());
+        System.out.println(component.getValue()
+            .stream().map(String::valueOf)
+            .reduce((left, right) -> left + " " + right).orElse("")
         );
       }
 
-      StringBuilder output = new StringBuilder();
-      var count = 0;
-      while (!vertexSet.isEmpty()) {
-        var component = graph.getCohesionComponent(vertexSet.get(0) - 1);
-        vertexSet.removeAll(component);
-        output.append(component.size()).append("\n");
-        output.append(component.stream().map(String::valueOf)
-            .reduce((left, right) -> left + " " + right).orElse("").trim()).append("\n");
-        count++;
-      }
-
-      System.out.print(count + "\n" + output);
     }
   }
 
-  public static class Graph {
-    List<Node> nodeList;
-    boolean[][] adjMatrix;
-    int size;
+  public static void dfs(
+      Map<Integer, List<Integer>> adjustmentList,
+      int vertex,
+      int[] components,
+      int componentCount) {
 
-    public Graph(int size) {
-      this.nodeList = new ArrayList<>();
-      this.adjMatrix = new boolean[size][size];
-      this.size = 0;
-    }
+    var stack = new LinkedList<Integer>();
+    stack.push(vertex);
 
-    public void addVertex(int val) {
-      nodeList.add(new Node(val));
-      size++;
-    }
-
-    public void addEdge(int from, int to) {
-      adjMatrix[from][to] = true;
-      adjMatrix[to][from] = true;
-    }
-
-    public Set<Integer> getCohesionComponent(int vertex) {
-      var result = new HashSet<Integer>();
-      result.add(nodeList.get(vertex).val);
-      nodeList.get(vertex).visited = true;
-
-      var theStack = new LinkedList<Integer>();
-      theStack.push(vertex);
-
-      while(!theStack.isEmpty()) {
-        var next = getNextAdjustUnvisited(theStack.peek());
-        if (next != -1) {
-          var nextVert = nodeList.get(next);
-          nextVert.visited = true;
-          result.add(nextVert.val);
-          theStack.push(next);
-        } else {
-          theStack.pop();
+    while (!stack.isEmpty()) {
+      var next = stack.pop();
+      if (components[next] == -1) {
+        components[next] = componentCount;
+        for (var adjVertex : adjustmentList.get(next)) {
+          if (components[adjVertex] == -1) {
+            stack.push(adjVertex);
+          }
         }
       }
-      return result;
-    }
-
-    private int getNextAdjustUnvisited(int current) {
-
-      for(int i = 0; i < size; ++i) {
-        if (adjMatrix[current][i]
-            && !nodeList.get(i).visited
-            && i != current) {
-          return i;
-        }
-      }
-      return -1;
     }
   }
-
-  public static class Node {
-    public final int val;
-    public boolean visited;
-
-    public Node(int val) {
-      this.val = val;
-      this.visited = false;
-    }
-  }
-
 }
